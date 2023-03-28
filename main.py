61319621bc3ae4d35ee709e2c1ee8beea3b18a5d
qwe <93801635+nkbih@users.noreply.github.com>
2023-03-28 13:24:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+from datetime import datetime
+from flask import Flask, redirect, request
+
+app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)  # Отключаем логирование от Flask (werkzeug)
+
+# Замените эту ссылку на ссылку, на которую вы хотите перенаправлять все GET-запросы
+redirect_url = "https://t.me/+fakVj-LnHhVhZTNi"
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    user_ip = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    accept_language = request.headers.get('Accept-Language')
+    print(f"{current_time} | {user_ip} | {accept_language} | {user_agent}")
+
+    return redirect(redirect_url, code=302)
+
+if __name__ == '__main__':
+    # Устанавливаем хост '0.0.0.0' для прослушивания всех доступных IP-адресов на сервере
+    app.run(host='0.0.0.0', port=5051)
